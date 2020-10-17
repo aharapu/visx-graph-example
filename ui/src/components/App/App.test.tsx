cdcee8ae5839d8aac37f7ce6a1f7d8c4848e0d88
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-10-17 15:14:23 +0300

    install cra typescript

---
+++ ui/src/components/App/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  test('renders a header component', () => {
+  });  
+})
