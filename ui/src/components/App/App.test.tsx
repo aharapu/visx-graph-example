4515a2a29bbc5e15588f75a7c48e8227141d41d3
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-10-17 16:10:57 +0300

    setup main components

--- ui/src/components/App/App.test.tsx
+++ ui/src/components/App/App.test.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+import Header from '../Header'
+import Data from '../Data'
 
 describe('App component', () => {
-  test('renders a header component', () => {
-  });  
+    const appWrapper = shallow(<App />)
+
+    it('renders a Header component', () => {
+        expect(appWrapper.find(Header)).toHaveLength(1)
+    })
+
+    it('renders a Data component', () => {
+        expect(appWrapper.find(Data)).toHaveLength(1)
+    })
 })
--- ui/src/components/App/App.tsx
+++ ui/src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header'
+import Data from '../Data'
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Header />
+      <Data />
+    </>
   );
 }
 
